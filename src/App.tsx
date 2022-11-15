35d012e27922863b048de74857b334d7b8ba8008
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2022-11-14 21:25:44 -0300

    feat(boilerplate): added the boilerplate structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './tailwind.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="App bg-red-700 ">
+      <header className="App-header bg">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
