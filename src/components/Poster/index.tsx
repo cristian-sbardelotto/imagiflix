a103c55af23c7ec76b3da75fb87d176f2179bdcc
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2022-11-21 18:47:16 -0300

    feat(score): created the score component

--- src/components/Poster/index.tsx
+++ src/components/Poster/index.tsx
@@ -4,14 +4,20 @@ import { Movie } from '../../data/mock';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlayCircle } from '@fortawesome/free-solid-svg-icons';
 
+import Score from '../Score/index';
+
 import './index.css';
 
 const Poster = ({ cover, title, score }: Movie, index: number) => (
-  <article className='relative transition-all duration-500 ease-in-out transform hover:scale-110 hover:z-10' key={index}>
+  <article
+    className='relative transition-all duration-500 ease-in-out transform hover:scale-110 hover:z-10'
+    key={index}
+  >
     <img src={cover} alt={title} className='h-72' />
-    <div className='poster absolute inset-0 h-full grid place-items-center w-full bg-black bg-opacity-75 transition-all duration-500 ease-in-out'>
+    <div className='poster absolute inset-0 h-full grid place-items-center px-4 w-full bg-black bg-opacity-75 transition-all duration-500 ease-in-out cursor-pointer'>
       <FontAwesomeIcon icon={faPlayCircle} size='5x' />
       <h2 className='text-2xl'>{title}</h2>
+      <Score value={score} />
     </div>
   </article>
 );
