3b1ae5f4f1f951509aad1f902cd08c6ef7c993c9
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2022-11-24 09:45:16 -0300

    feat(api): added the popular film to the hero, bug in the render

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import CONST from './data/constants';
 
 import Hero from './components/Hero/index';
 import NavBar from './components/Navbar/index';
@@ -10,11 +12,30 @@ import 'slick-carousel/slick/slick-theme.css';
 import './tailwind.css';
 
 const App = () => {
+  const { URL, APISTRING } = CONST;
+
+  const [movies, setMovies] = useState<any[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(
+        `${URL}/discover/movie${APISTRING}&sort_by=popularity.desc`
+      );
+
+      const data = await response.json();
+
+      setMovies(data);
+    };
+
+    fetchData();
+  }, []);
+
+  useEffect(() => movies && console.log(movies), [movies]);
+
   return (
     <div className='m-auto antialiased font-sans bg-black text-white'>
       <NavBar />
-      <Hero />
-      <Carousel />
+      <Hero {...movies?.results[0]} />
       <Carousel />
       <Carousel />
       <Carousel />
---
+++ src/data/constants.tsx
@@ -0,0 +1,11 @@
+const URL = 'https://api.themoviedb.org/3';
+const IMAGEURL = 'https://image.tmdb.org/t/p';
+const APIKEY = '41b09a1ab2389780ca5d1aa46c1fcf95';
+const APISTRING = `?api_key=${APIKEY}&language=pt-BR`;
+
+export default {
+  URL,
+  IMAGEURL,
+  APIKEY,
+  APISTRING
+};
